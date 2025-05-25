bb4d08e663923fe9efa0e8ad5d68c652f8446a0a
Muhammad Hamza <150447026+hamzaentterprice@users.noreply.github.com>
2025-05-25 13:14:11 +0500

    ddd

--- src/components/VerificationForm.tsx
+++ src/components/VerificationForm.tsx
@@ -335,7 +335,7 @@ export const VerificationForm = () => {
             />
           </div>
 
-          {/* <div>
+          <div>
             <Label htmlFor="verificationCode">Verification Code</Label>
             <Input
               id="verificationCode"
@@ -345,7 +345,7 @@ export const VerificationForm = () => {
               onChange={(e) => setVerificationCode(e.target.value)}
               required
             />
-          </div> */}
+          </div>
 
           <Button 
             type="submit" 
